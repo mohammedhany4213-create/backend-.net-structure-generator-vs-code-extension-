34e2012660d1c6383f92dccad699a04af63f5acf
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-07 16:24:34 +0300

    chore: improve extension activation logging

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,14 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Backend Structure Generator activated!');
 
     const disposable = vscode.commands.registerCommand(
         'backend-structure-generator.generate',
         async () => {
-
             vscode.window.showInformationMessage(
                 'Backend Structure Generator is working!'
             );
-
         }
     );
 
